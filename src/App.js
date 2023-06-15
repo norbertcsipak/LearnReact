26dcdb1a1d94990b79a627b00dda821bb68c0ded
Norbert Csipak <norbertcipak@me.com>
2023-06-15 09:23:31 +0100

    Multiple components in React

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 
-function App() {
-  console.log('Hello from the component...')
-  const now = new Date();
-  const a = 10;
-  const b = 20;
+// function App() {
+//   console.log('Hello from the component...')
+//   const now = new Date();
+//   const a = 10;
+//   const b = 20;
 
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+//   return React.createElement(
+//     'div',
+//     null,
+//     React.createElement(
+//       'p', null, 'Hello world, it is ', now.toString()
+//     ),
+//     React.createElement(
+//       'p', null, a, ' plus ', b, ' is ', a + b
+//     )
+//   )
+// }
+
+function App() {
+  return(
+    <div>
+      <h1>Greetings</h1>
+      <Hello message='Hope you are having a lovely day!'/>
+    </div>
+  )
+}
+
+const Hello = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <p>Hello there! {props.message}</p>
+    </div>
   )
 }
 
